43b71fd5cb1579a1aeb0a210890e16595d4fb41e
pratik-karna <kaystha.pratik@gmail.com>
2023-08-06 12:30:02 +0530

    updated code

---
+++ Basic/ConditionalStatement/StudentPassOrFail.java
@@ -0,0 +1,11 @@
+package Basic.ConditionalStatement;
+
+public class StudentPassOrFail {
+    public static void main(String[] args) {
+        int mark = 40;
+        String reportCard = mark >= 50 ? "Pass" : "Fail";
+        System.out.println(reportCard);
+
+    }
+
+}
---
+++ Basic/ConditionalStatement/SwitchStatement.java
@@ -0,0 +1,21 @@
+package Basic.ConditionalStatement;
+
+public class SwitchStatement {
+    public static void main(String[] args) {
+        int number = 2;
+        switch (number) {
+            case 1:
+                System.out.println("School");
+                break;
+
+            case 2:
+                System.out.println("College");
+                break;
+
+            case 3:
+                System.out.println("University");
+
+        }
+    }
+
+}
