07547c7ddff515d147adc1a5a1cb0acb062d0e2a
pratik-karna <kaystha.pratik@gmail.com>
2023-08-07 22:42:08 +0530

    Updated code

--- Basic/ConditionalStatement/Calculator.java
+++ Basic/ConditionalStatement/Calculator.java
@@ -1,37 +1,42 @@
 package Basic.ConditionalStatement;
 
+import java.io.*;
 import java.util.*;
 
 public class Calculator {
-    public static void main(String[] args)
-    {
+    public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
+
+        System.out.print("Enter a: ");
         int a = sc.nextInt();
-        System.out.println("Enter the first Number: ");
+        System.out.print("Enter b: ");
         int b = sc.nextInt();
-        System.out.println("Enter the second Number: ");
+        System.out.print("Enter operator: ");
         char operator = sc.next().charAt(0);
-        
+
         switch (operator) {
+
             case '+':
-                System.out.println("a + b");
+                System.out.print(a + b);
                 break;
 
             case '-':
-                System.out.println("a - b");
+                System.out.print(a - b);
                 break;
 
             case '*':
-                System.out.println("a * b");
-            
+                System.out.print(a * b);
+                break;
+
             case '/':
-                System.out.println("a / b");
+                System.out.print(a / b);
+                break;
 
+            default:
+                System.out.print("Invalid Operator");
 
-        sc.close();
-
-        
         }
+        sc.close();
 
     }
 
