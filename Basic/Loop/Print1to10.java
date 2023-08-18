43b9e5ee17621ca4ed957eba72586178f4ac3b4d
pratik-karna <kaystha.pratik@gmail.com>
2023-08-18 16:21:10 +0530

    Updated code

--- Basic/Loop/Print1to10.java
+++ Basic/Loop/Print1to10.java
@@ -2,7 +2,7 @@ package Basic.Loop;
 
 public class Print1to10 {
     public static void main(String[] args){
-        int counter = 1;
+        int counter =1;
         while(counter <= 10){
             System.out.print(counter+" ");
             counter++;
---
+++ Basic/Loop/PrintNumberFrom1ton.java
@@ -0,0 +1,17 @@
+package Basic.Loop;
+
+import java.util.*;
+
+public class PrintNumberFrom1ton {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int range = sc.nextInt();
+        int counter = 1;
+        while (counter <= range) {
+            System.out.println(counter + " ");
+            sc.close();
+        }
+        System.out.println();
+    }
+
+}
