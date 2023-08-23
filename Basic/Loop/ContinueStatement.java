d24c93cfd97ee39d99d4bfe9e072dec47adc8bea
pratik-karna <kaystha.pratik@gmail.com>
2023-08-23 17:15:07 +0530

    Updated code with easy understand

---
+++ Basic/Loop/ContinueStatement.java
@@ -0,0 +1,14 @@
+package Basic.Loop;
+
+public class ContinueStatement {
+    public static void main(String[] args){
+        for(int i=1; i<=5; i++){
+            if(i == 1){
+                continue;
+            }
+            System.out.println(i);
+        }
+
+    }
+    
+}
