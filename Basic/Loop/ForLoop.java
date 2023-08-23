f0995242d59f81c14097952d6e97d2e629803e1c
pratik-karna <kaystha.pratik@gmail.com>
2023-08-23 15:10:22 +0530

    updated Code

---
+++ Basic/Loop/ForLoop.java
@@ -0,0 +1,11 @@
+package Basic.Loop;
+
+public class ForLoop {
+    public static void main(String[] args){
+        
+        for(int i=1; i<= 10; i++){
+            System.out.println("Pratik Karna"); 
+        }
+    }
+
+}
---
+++ Basic/Loop/PrintSquarePattern.java
@@ -0,0 +1,30 @@
+package Basic.Loop;
+
+public class PrintSquarePattern {
+    public static void main(String[] args){
+
+      //This is code for for loop
+
+        for(int i=1; i<=4; i++){
+            System.out.println("* * * *");
+        }
+
+        //This is code for for loop
+
+        int i = 1;
+        while(i <= 4){
+            System.out.println("* * * *");
+            i++;
+
+        }
+    }
+    
+}
+//Output
+
+/*
+* * * *
+* * * *
+* * * *
+* * * *
+*/
