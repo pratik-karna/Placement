07780810d4739c7c1786ca9d8a00651e12f029f7
pratik-karna <kaystha.pratik@gmail.com>
2023-08-24 23:25:15 +0530

    Updated Code

--- Basic/Variable/Addition.java
+++ Basic/Variable/Addition.java
@@ -1,8 +1,6 @@
 import java.util.Scanner;
-
 public class Addition {
     public static void main(String[] args) {
-
         Scanner sc = new Scanner(System.in);
         System.out.print("Enter the first number: ");
         int a = sc.nextInt();
