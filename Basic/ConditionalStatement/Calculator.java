44ce4912794271a8da22f36ddf1a92518605b95d
pratik-karna <kaystha.pratik@gmail.com>
2023-08-24 23:33:03 +0530

    Updated Code

--- Basic/ConditionalStatement/Calculator.java
+++ Basic/ConditionalStatement/Calculator.java
@@ -1,11 +1,8 @@
 package Basic.ConditionalStatement;
-
 import java.util.*;
-
 public class Calculator {
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
-
         System.out.print("Enter a: ");
         int a = sc.nextInt();
         System.out.print("Enter b: ");
