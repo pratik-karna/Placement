390c8b210e77c4aaf111a347ac5087ec69122836
pratik-karna <kaystha.pratik@gmail.com>
2023-08-26 15:21:35 +0530

    Updated Code

---
+++ Basic/Function/HelloWorld.java
@@ -0,0 +1,11 @@
+package Basic.Function;
+
+public class HelloWorld {
+    public static void printHelloWorld() {
+    System.out.print("Hello World");
+}
+    public static void main(String[] args){
+        printHelloWorld(); //this is function call in java
+    }
+    
+}
