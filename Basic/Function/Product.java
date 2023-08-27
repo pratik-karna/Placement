789c267b4a1ddc6ee5ca05175f36ce30acac8f74
pratik-karna <kaystha.pratik@gmail.com>
2023-08-27 16:23:12 +0530

    Updated Code

---
+++ Basic/Function/Product.java
@@ -0,0 +1,21 @@
+package Basic.Function;
+import java.util.*;
+
+public class Product {
+    public static int multiply(int a, int b){
+    int multiply = a * b;
+    System.out.println("Multiple is: "+ multiply);
+    return multiply;
+    }
+    public static void main(String[] args){
+        Scanner sc = new Scanner(System.in);
+        System.out.println("Enter the a: ");
+        int a = sc.nextInt();
+        System.out.println("Enter the b: ");
+        int b = sc.nextInt();
+        multiply(a, b);
+        sc.close();
+        
+    }
+    
+}
