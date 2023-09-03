024378a313b6ade1eb1a09acff3a939c42046204
pratik-karna <kaystha.pratik@gmail.com>
2023-09-03 11:11:10 +0530

    updated code

--- Arrays/ArraysCreation.java
+++ Arrays/ArraysCreation.java
@@ -3,5 +3,7 @@ public class ArraysCreation {
     public static void main(String[] args){
         int marks[] = new int[50];
         int numbers[] = {1, 2, 3};
+        System.out.println(marks[0]);
+        System.out.println(numbers[123]);
     }
 }
--- Arrays/LargestNumberArray.java
+++ Arrays/LargestNumberArray.java
@@ -1,12 +1,11 @@
 package Arrays;
-
 public class LargestNumberArray {
 
     public static int getLargest(int numbers[]){
         int largest = Integer.MIN_VALUE;
 
         for(int i=0; i<numbers.length; i++){
-            if(largest < numbers[i]);
+            if(largest < numbers[i])
             largest = numbers[i];
         }
         return largest;
@@ -15,7 +14,7 @@ public class LargestNumberArray {
 
     public static void main(String[] args)
     {
-         int numbers[] = {3, 4, 5, 20, 10, 8, 9, 11, 17};
+         int numbers[] = {3, 4, 5, 10, 8, 20, 9, 11, 17, 13, 12};
          System.out.print("largest value is :"+ getLargest(numbers));
 
     }
