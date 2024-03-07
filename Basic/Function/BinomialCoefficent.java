28d00539f567f6a6e7007cf450d446731cf4f6a8
pratik-karna <kaystha.pratik@gmail.com>
2024-03-07 18:45:35 +0530

    First commit

---
+++ Basic/Function/BinomialCoefficent.java
@@ -0,0 +1,12 @@
+package Basic.Function;
+
+public class BinomialCoefficent {
+    public static int buncoed(int n, int r){
+        return r;
+        // method implementation goes here
+    }
+
+    public static void main(String[] args){
+
+    }
+}
