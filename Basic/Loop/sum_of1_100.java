8c29948a92723a11f27ff05263e03c0b438e92e4
pratik-karna <kaystha.pratik@gmail.com>
2024-03-15 23:03:22 +0530

    Updated code

---
+++ Basic/Loop/sum_of1_100.java
@@ -0,0 +1,12 @@
+package Basic.Loop;
+
+public class sum_of1_100 {
+    public static void main(String args[]){
+        int sum = 0; 
+        for(int i = 1; i <= 100; i++){
+            sum =  sum + i; 
+            System.out.println(sum);
+        }
+    }
+    
+}
