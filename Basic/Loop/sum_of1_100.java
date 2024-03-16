a0470594b2507ba201bfd223dba879228d147ac9
pratik-karna <kaystha.pratik@gmail.com>
2024-03-16 14:20:02 +0530

    updated code

--- Basic/Loop/sum_of1_100.java
+++ Basic/Loop/sum_of1_100.java
@@ -4,7 +4,7 @@ public class sum_of1_100 {
     public static void main(String args[]){
         int sum = 0; 
         for(int i = 1; i <= 100; i++){
-            sum =  sum + i; 
+            sum +=i; 
             System.out.println(sum);
         }
     }
