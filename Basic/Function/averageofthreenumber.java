01db5467557ecaff65e2d5eb77cd90db311b343a
pratik-karna <kaystha.pratik@gmail.com>
2024-03-18 22:01:13 +0530

    updated code

---
+++ Basic/Function/averageofthreenumber.java
@@ -0,0 +1,12 @@
+package Basic.Function;
+
+public class averageofthreenumber {
+    public static void main(String args[]){
+        int a = 10;
+        int b = 20;
+        int c = 30;
+        int avg = (a+b+c)/3;
+        System.out.println(avg);
+    }-
+    
+}
