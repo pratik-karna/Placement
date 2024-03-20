de2b06e8e75cc3627de10fe81cfb04fcfa303b27
pratik-karna <kaystha.pratik@gmail.com>
2024-03-20 11:40:34 +0530

    updated code

--- Basic/Loop/WhileLoop.java
+++ Basic/Loop/WhileLoop.java
@@ -2,10 +2,10 @@ package Basic.Loop;
 public class WhileLoop {
     public static void main(String[] args){
         int counter = 0;
-        while(counter < 500 ){
+        while(counter < 10 ){
         System.out.println("Pratik");
         counter++;}    
-        System.out.println("Printed Pratik 100x");
+        System.out.println("Printed Pratik");
     }
     
 }
