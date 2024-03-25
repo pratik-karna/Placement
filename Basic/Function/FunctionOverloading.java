310f99520935236abecb86cb32dd7f2c586b3e24
pratik-karna <kaystha.pratik@gmail.com>
2024-03-25 18:52:52 +0530

    first commit

--- Basic/Function/FunctionOverloading.java
+++ Basic/Function/FunctionOverloading.java
@@ -15,7 +15,7 @@ public class FunctionOverloading {
     }
 
     public static void main(String args[]){
-        System.out.println("Addition is: "+ sum(10, 15, 25)); //calling the two integer addition 
-        System.out.println("Addition is: " +sum(5,10));   //calling the function for adding the three integer
+        System.out.println("Addition of two number: "+ sum(5,10)); //calling the two integer addition 
+        System.out.println("Addition of three number: " +sum(10,20,30));   //calling the function for adding the three integer
     }
 }
