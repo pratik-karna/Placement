0da6e1ecb72373a431026f4f5da7326e20b6bea3
pratik-karna <kaystha.pratik@gmail.com>
2024-03-25 19:06:46 +0530

    first updated

--- Basic/Function/FunctionOverloading.java
+++ Basic/Function/FunctionOverloading.java
@@ -1,15 +1,19 @@
 package Basic.Function;
 
 
-// In methodoverloading we can only change the inside method parameter not the written type
+// In methodoverloading we can only change the inside method of parameter not the written type
 
 
 public class FunctionOverloading {
+
+     //Function to calculate the two integer values
 
     public static int sum(int a, int b){
         return a+b;
     }
     
+    //Function to calculate the three integer values
+
     public static int sum(int a, int b, int c){
         return a+b+c;
     }
@@ -18,4 +22,25 @@ public class FunctionOverloading {
         System.out.println("Addition of two number: "+ sum(5,10)); //calling the two integer addition 
         System.out.println("Addition of three number: " +sum(10,20,30));   //calling the function for adding the three integer
     }
+
+
+
+
+//Using different Data Types
+    
+
+    public static int sum(int a, int b){
+        return a+b;
+    }
+
+    public static float sum(float a, float b){
+        return a+b;
+    }
+
+    public static void main(String args[]){
+        System.out.println("Addition of two number:"+ sum(10,10));
+        System.out.println("Addition of two float values:"+ sum(9.8f, 10.2f));
+    }
+
+
 }
