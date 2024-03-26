c5615f9456c7bf5221d346c2693bdd7836bf280f
pratik-karna <kaystha.pratik@gmail.com>
2024-03-26 10:25:36 +0530

    first commit

---
+++ Basic/Function/PrimeInRange.java
@@ -0,0 +1,31 @@
+package Basic.Function;
+
+public class PrimeInRange{
+    int n;
+
+    public static boolean isPrime(int num) {
+        if (num <= 1) {
+            return false;
+        }
+        for (int i = 2; i <= Math.sqrt(num); i++) {
+            if (num % i == 0) {
+                return false;
+            }
+        }
+        return true;
+    }
+
+    public static void PrimesInRange(int n) {
+        for (int i = 2; i <= n; i++) {
+            if (isPrime(i)) {
+                System.out.print(i + " ");
+            }
+        }
+        System.out.println();
+    }
+
+    public static void main(String args[]) {
+        PrimesInRange(16);
+    }
+    
+}
